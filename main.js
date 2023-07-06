e8811600f515472a8d9299516b2455274cf4d6f1
rafaelrvs <rafaelvieira1333@gmail.com>
2023-07-06 20:30:26 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+class Cliente{
+  nome;
+  cpf;
+  agencia;
+  saldo;
+}
+
+const cliente1=  new Cliente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = 1234567891011;
+cliente1.agencia = 1241;
+cliente1.saldo = 20.01;
+
+const cliente2=  new Cliente();
+cliente2.nome = "joana";
+cliente2.cpf = 4505115115415;
+cliente2.agencia = 1001;
+cliente2.saldo = 0.01;
+
+console.log(cliente1)
+console.log(cliente2)
+
+
